29020d3ab1fdef30ae7a90a5f1cae1b3cc0af9cc
Himanshu Choudhary <baliyanh10@gmail.com>
2026-09-13 00:06:47 +0530

    routing get and post

--- src/app.js
+++ src/app.js
@@ -6,13 +6,21 @@ app.use("/home",(req,res)=>{
     res.send('Hello World from Express.js new!');
 })
 
-app.use("/",(req,res)=>{
-    res.send('Hello World from Express.js!');
-})
-
 app.use("/about",(req,res)=>{
     res.send('About Page');
 })
+
+app.get("/contact",(req,res)=>{
+    res.send({firstName: "Himanshu", lastName: "Choudhary"});
+})
+
+app.post("/contact",(req,res)=>{
+    res.status(201).send({firstName: "Himanshu", lastName: "Choudhary"});
+})
+
+// app.use("/",(req,res)=>{
+//     res.send('Hello World from Express.js!');
+// })
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
