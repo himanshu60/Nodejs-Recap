1cf8487979a8336558c928a6446a2ceb63458242
Himanshu Choudhary <baliyanh10@gmail.com>
2026-09-13 11:19:17 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -14,9 +14,25 @@ app.get("/contact",(req,res)=>{
     res.send({firstName: "Himanshu", lastName: "Choudhary"});
 })
 
-app.post("/contact",(req,res)=>{
-    res.status(201).send({firstName: "Himanshu", lastName: "Choudhary"});
+app.get("/user/:id",(req,res)=>{
+    res.send({firstName: "Himanshu", lastName: "Choudhary"});
 })
+
+// 
+// What you want	Express 4 (old tutorials)	Express 5
+// One or more b	"/ab+c"	/^\/ab+c$/ (regex)
+// Optional b	"/ab?c"	"/a{b}c"
+// Optional group	"/a(bc)?d"	"/a{bc}d"
+// Wildcard	"/ab*cd"	"/ab*splat" (wildcard must have a name)
+// Match everything	"*"	"/*splat" or "/{*splat}"
+// Route param	"/user/:id"	"/user/:id" (unchanged)
+// Regex routes like /a/ (any path containing "a") or /.*fly$/ work the same in both versions.
+
+app.get(/^\/ab+c$/,(req,res)=>{
+    res.send({firstName: "Himanshu", lastName: "Choudhary"});
+})
+
+
 
 // app.use("/",(req,res)=>{
 //     res.send('Hello World from Express.js!');
