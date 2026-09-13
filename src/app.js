cb73bf477df49a80bb1755996e7e79dd7930f687
Himanshu Choudhary <baliyanh10@gmail.com>
2026-09-13 12:20:37 +0530

    seprate middlewares for each routes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use("/user",(req,res,next)=>{
-    // res.send("User route");
-    console.log("User route middleware");
-    next();
-},(req,res)=>{
-    console.log("User route handler");
-    res.send("User route 2");
-})
+
+app.get("/admin/getAllData", (req, res) => {
+  const token = "xyz123"; // Replace with your actual token
+  const isAuthorized = token === "x1yz123"; // Replace with your actual authorization logic
+
+  if (!isAuthorized) {
+    return res.status(403).send("Unauthorized access");
+  }
+  res.send("All data retrieved");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
