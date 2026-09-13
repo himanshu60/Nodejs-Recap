eb5a045730c4047c31bd80a9b618f5cb0f8d1d87
Himanshu Choudhary <baliyanh10@gmail.com>
2026-09-13 11:48:54 +0530

    middleware and route handler by using next func

--- src/app.js
+++ src/app.js
@@ -2,41 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use("/home",(req,res)=>{
-    res.send('Hello World from Express.js new!');
+app.use("/user",(req,res,next)=>{
+    // res.send("User route");
+    console.log("User route middleware");
+    next();
+},(req,res)=>{
+    console.log("User route handler");
+    res.send("User route 2");
 })
-
-app.use("/about",(req,res)=>{
-    res.send('About Page');
-})
-
-app.get("/contact",(req,res)=>{
-    res.send({firstName: "Himanshu", lastName: "Choudhary"});
-})
-
-app.get("/user/:id",(req,res)=>{
-    res.send({firstName: "Himanshu", lastName: "Choudhary"});
-})
-
-// 
-// What you want	Express 4 (old tutorials)	Express 5
-// One or more b	"/ab+c"	/^\/ab+c$/ (regex)
-// Optional b	"/ab?c"	"/a{b}c"
-// Optional group	"/a(bc)?d"	"/a{bc}d"
-// Wildcard	"/ab*cd"	"/ab*splat" (wildcard must have a name)
-// Match everything	"*"	"/*splat" or "/{*splat}"
-// Route param	"/user/:id"	"/user/:id" (unchanged)
-// Regex routes like /a/ (any path containing "a") or /.*fly$/ work the same in both versions.
-
-app.get(/^\/ab+c$/,(req,res)=>{
-    res.send({firstName: "Himanshu", lastName: "Choudhary"});
-})
-
-
-
-// app.use("/",(req,res)=>{
-//     res.send('Hello World from Express.js!');
-// })
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
