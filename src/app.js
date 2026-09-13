67183cee6e56934c9c7015b7afba6041354cd795
Himanshu Choudhary <baliyanh10@gmail.com>
2026-09-13 19:57:32 +0530

    update userbyid

--- src/app.js
+++ src/app.js
@@ -55,6 +55,44 @@ app.get("/oneUser", async (req, res) => {
     }
 })
 
+app.delete("/deleteUser", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        if (!userId) {
+            return res.status(400).json({ message: "User ID is required" });
+        }
+        else {
+            const deletedUser = await User.findByIdAndDelete(userId);
+            if (!deletedUser) {
+                return res.status(404).json({ message: "User not found" });
+            } else {
+                res.status(200).json({ message: "User deleted successfully", user: deletedUser });
+            }
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Error occurred while deleting user" });
+    }
+});
+
+app.patch("/updateUser", async (req, res) => {
+    const userId = req.body.userId;
+    const updateData =req.body
+    try {
+        if (!userId) {
+            res.status(404).send({ message: "userId not found" })
+        } else {
+            const updateUser = await User.findByIdAndUpdate(userId, updateData, { returnDocument: "after" });
+            if (!updateUser) {
+                res.status(404).send({ message: "userId not found" })
+            } else {
+                res.status(200).send({ message: "user updated sucessfully", updateUser })
+            }
+        }
+    } catch (err) {
+
+    }
+})
+
 connectDB().then(() => {
     console.log("Database connected successfully");
     app.listen(port, () => {
