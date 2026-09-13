4b79161bcd219c3835b50631e1fb5c852f74d6a0
Himanshu Choudhary <baliyanh10@gmail.com>
2026-09-13 19:16:15 +0530

    getusers

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({quiet: true});
+require('dotenv').config({ quiet: true });
 const express = require('express');
 const { connectDB } = require("./config/database")
 const app = express();
@@ -7,14 +7,51 @@ const User = require("./models/user");
 const port = process.env.PORT || 3000;
 
 app.post("/signup", async (req, res) => {
-    const data =req.body
+    const data = req.body
     // console.log("Received signup data:", data);
     const user = new User(data);
     try {
         await user.save();
         res.status(201).json({ message: "User created successfully", user });
     } catch (err) {
-        res.status(500).json({ message: "Error occurred while signing up" });
+        res.status(500).json({ message: "Error occurred while signing up", error: err.message });
+    }
+})
+
+app.get("/users", async (req, res) => {
+    const users = req.body.emailId;
+    try {
+        if (!users || users.length === 0) {
+            return res.status(400).json({ message: "No email IDs provided" });
+        } else {
+            const foundUsers = await User.find({ emailId: users });
+            res.status(200).json({ message: "Users found successfully", foundUsers });
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Error occurred while fetching users" });
+    }
+});
+
+app.get("/allUsers", async (req, res) => {
+    try {
+        const allUsers = await User.find({});
+        res.status(200).json({ message: "All users found successfully", users: allUsers });
+    } catch (err) {
+        res.status(500).json({ message: "Error occurred while fetching all users" });
+    }
+})
+
+app.get("/oneUser", async (req, res) => {
+    const email = req.body.emailId;
+    try {
+        if (!email) {
+            return res.status(400).json({ message: "Email ID is required" });
+        } else {
+            const oneUser = await User.findOne({ emailId: email });
+            res.status(200).json({ message: "All users found successfully", users: oneUser });
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Error occurred while fetching all users" });
     }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    emailId: { type: String, required: true, unique: true },
+    emailId: { type: String, required: true },
     password: { type: String, required: true },
     age: { type: Number, required: true },
     gender: { type: String, required: true },
