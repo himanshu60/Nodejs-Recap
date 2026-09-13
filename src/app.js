0f814e716956f13f845a3156335f8537af217c05
Himanshu Choudhary <baliyanh10@gmail.com>
2026-09-13 13:14:00 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const {adminAuth,userAuth } = require('./middlewares/auth');
 
 app.use("/admin", adminAuth);
 
+
 app.get("/admin/getAllData", (req, res) => {
   res.send("All data retrieved");
 });
@@ -12,6 +13,22 @@ app.get("/admin/getAllData", (req, res) => {
 app.get("/user/getUserData",userAuth, (req, res) => {
   res.send("User data retrieved");
 });
+
+app.get("/login", (req, res) => {
+    try{
+        throw new Error("Simulated error in login route"); // Simulate an error for testing 
+        res.send("Login page");
+    }catch(err){
+        // console.error(err.stack);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+
+app.use("/",(err,req,res,next)=>{
+    // console.error(err.stack);
+    res.status(500).send("Internal Server Error from global error handler");
+})
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
